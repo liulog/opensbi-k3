9d8cb995958031cea60b3951db2fa3cd2e8d2a38
Nell <xianbin.zhu@spacemit.com>
2025-05-27 10:10:04 +0800

    rpmi: support votage gourp
    Change-Id: Ie7ef3213ba04fde8a443fcc49b3f0287f522cf5e
    Signed-off-by: Nell <xianbin.zhu@spacemit.com>

--- lib/utils/mpxy/objects.mk
+++ lib/utils/mpxy/objects.mk
@@ -15,5 +15,8 @@ libsbiutils-objs-$(CONFIG_FDT_MPXY_RPMI_MBOX) += mpxy/fdt_mpxy_rpmi_mbox.o
 carray-fdt_mpxy_drivers-$(CONFIG_FDT_MPXY_RPMI_CLOCK) += fdt_mpxy_rpmi_clock
 libsbiutils-objs-$(CONFIG_FDT_MPXY_RPMI_CLOCK) += mpxy/fdt_mpxy_rpmi_clock.o
 
+carray-fdt_mpxy_drivers-$(CONFIG_FDT_MPXY_RPMI_VOLTAGE) += fdt_mpxy_rpmi_voltage
+libsbiutils-objs-$(CONFIG_FDT_MPXY_RPMI_VOLTAGE) += mpxy/fdt_mpxy_rpmi_voltage.o
+
 carray-fdt_mpxy_drivers-$(CONFIG_FDT_MPXY_RPMI_SYSMSI) += fdt_mpxy_rpmi_sysmsi
 libsbiutils-objs-$(CONFIG_FDT_MPXY_RPMI_SYSMSI) += mpxy/fdt_mpxy_rpmi_sysmsi.o
