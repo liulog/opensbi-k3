3535c2722fb91a0a0288521fd8b227d7e8703407
Nell <xianbin.zhu@spacemit.com>
2026-01-27 10:31:47 +0800

    k3:increase the size of scratch for cpuidle context
    Change-Id: I1474334fed220807b867a269e6813b34ef0171f7
    Signed-off-by: Nell <xianbin.zhu@spacemit.com>

--- include/sbi/sbi_scratch.h
+++ include/sbi/sbi_scratch.h
@@ -46,8 +46,8 @@
 #define SBI_SCRATCH_HARTINDEX_OFFSET		(14 * __SIZEOF_POINTER__)
 /** Offset of extra space in sbi_scratch */
 #define SBI_SCRATCH_EXTRA_SPACE_OFFSET		(15 * __SIZEOF_POINTER__)
-/** Maximum size of sbi_scratch (4KB) */
-#define SBI_SCRATCH_SIZE			(0x1000)
+/** Maximum size of sbi_scratch (8KB) */
+#define SBI_SCRATCH_SIZE			(0x2000)
 
 /* clang-format on */
 
