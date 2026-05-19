e4d27728269c24d26f5d526d1a68e91dcdcb0e08
Nell <xianbin.zhu@spacemit.com>
2026-05-19 21:16:59 +0800

    k3:pmp: Protect certain address spaces.
    Set the rcpu runtime address space, rcpu DTB address space,
    and RPMI address space to be inaccessible by M/S/U modes.
    
    the PMP configuraion like below:
    
    Domain0 Region00            : 0x00000000cac90c00-0x00000000cac90cff M: (I,R,W) S/U: ()
    Domain0 Region01            : 0x00000000d4017000-0x00000000d4017fff M: (I,R,W) S/U: (R,W)
    Domain0 Region02            : 0x00000000f1800000-0x00000000f1803fff M: (I,R,W) S/U: ()
    Domain0 Region03            : 0x0000000100e00000-0x0000000100e03fff M: (I,R,W) S/U: ()
    Domain0 Region04            : 0x00000000f1000000-0x00000000f100ffff M: (I,R,W) S/U: ()
    Domain0 Region05            : 0x00000000f1810000-0x00000000f181ffff M: (I,R,W) S/U: ()
    Domain0 Region06            : 0x0000000100000000-0x000000010003ffff M: (R,X) S/U: ()
    Domain0 Region07            : 0x0000000100000000-0x00000001000fffff M: (R,W) S/U: ()
    Domain0 Region08            : 0x0000000100600000-0x00000001006fffff M: () S/U: ()
    Domain0 Region09            : 0x0000000100c00000-0x0000000100cfffff M: () S/U: ()
    Domain0 Region10            : 0x0000000100f00000-0x0000000100ffffff M: () S/U: ()
    Domain0 Region11            : 0x0000000100200000-0x00000001003fffff M: () S/U: ()
    Domain0 Region12            : 0x0000000100400000-0x00000001005fffff M: () S/U: ()
    Domain0 Region13            : 0x0000000101000000-0x00000001011fffff M: () S/U: ()
    Domain0 Region14            : 0x0000000100800000-0x0000000100bfffff M: () S/U: ()
    Domain0 Region15            : 0x0000000000000000-0xffffffffffffffff M: () S/U: (R,W,X)
    
    The above configuration can protect the OpenSBI runtime, RCPU runtime, RPMI message passing shared buffer, and RCPU DTB space.
    
    rcpu0 runtime space [0x0000000100200000 0x0000000100700000]
    rcpu1 runtime space [0x0000000100800000 0x0000000100D00000]
    rcpu dtb space [0x0000000100f00000 0x0000000101200000]
    
    Signed-off-by: Nell <xianbin.zhu@spacemit.com>
    Change-Id: I5a73202cf2e5ae883a7d0dcf6075a3b1a33f3207

--- platform/generic/spacemit/spacemit_k3.c
+++ platform/generic/spacemit/spacemit_k3.c
@@ -17,96 +17,12 @@
 #include <sbi/sbi_ipi.h>
 #include <sbi/sbi_ecall_interface.h>
 #include <sbi/sbi_scratch.h>
+#include <sbi/sbi_domain.h>
 #include <sbi/sbi_timer.h>
 #include <sbi_utils/fdt/fdt_helper.h>
 #include <spacemit/spacemit_config.h>
-#include <spacemit/k3/k3_pmp.h>
 #include <sbi_utils/cache/cache.h>
 #include <sbi_utils/cci/cci.h>
-
-void spacemit_k3_pmp_init(void)
-{
-	int i, region_num;
-	PmpRegion* pRegion;
-
-	uint8_t pmpxcfg = 0;
-	uint64_t addr = 0, base_addr;
-	uint64_t region_size;
-	mpu_region_attr_t attr = { 0 };
-	int32_t region_size_id;
-
-	region_num = sizeof(pmp_setting) / sizeof(PmpRegion);
-	if (region_num > PMP_ENTRY_NUM) {
-		sbi_printf("region number exceed max!\n");
-		return;
-	}
-
-	/* setup pmp for each region */
-	for (i = 0; i < region_num; i++) {
-		pmpxcfg = 0;
-		pRegion = &pmp_setting[i];
-		base_addr = pRegion->start_addr;
-		region_size = pRegion->end_addr - base_addr + 1;
-		region_size_id = get_region_size_id(region_size);
-		if (!pRegion->region_enable)
-			attr.a = 0;
-		if (region_size_id == REGION_SIZE_4B) {
-			addr = base_addr >> 2;
-			attr.a = 2;
-		} else if (region_size_id != INVALID_REGION_SIZE_ID) { // use NAPOT
-			attr.a = 3;
-			addr = ((base_addr >> 2) & (0xFFFFFFFFFFU - ((1 << (region_size_id + 1)) - 1))) |
-				(((uint64_t)1 << region_size_id) - 1);
-		} else { // use TOR
-			attr.a = 1;
-			addr = (pRegion->end_addr + 1) >> 2;
-			if (i > 0 && pRegion->end_addr < pmp_setting[i - 1].end_addr) {
-				sbi_printf("Opps!If use TOR please make sure region address are increased!\n");
-				sbi_printf("Hang here unless fix me!\n");
-				while (1)
-					;
-			}
-		}
-		__set_PMPADDRx(i, addr);
-
-		attr.l = 1; // always lock which means also take effect for machine mode!!!
-		switch (pRegion->region_attr) {
-		case RWX:
-			attr.r = 1;
-			attr.w = 1;
-			attr.x = 1;
-			break;
-		case RWnX:
-			attr.r = 1;
-			attr.w = 1;
-			attr.x = 0;
-			break;
-		case ROX:
-			attr.r = 1;
-			attr.w = 0;
-			attr.x = 1;
-			break;
-		case ROnX:
-			attr.r = 1;
-			attr.w = 0;
-			attr.x = 0;
-			break;
-		case nRnWnX:
-			attr.r = 0;
-			attr.w = 0;
-			attr.x = 0;
-			break;
-		default:
-			sbi_printf("Opps! Unsupported PMP attribute!\n");
-			while (1)
-				;
-			break;
-		}
-		pmpxcfg |= (attr.r << PMP_PMPCFG_R_Pos) | (attr.w << PMP_PMPCFG_W_Pos) |
-			(attr.x << PMP_PMPCFG_X_Pos) | (attr.a << PMP_PMPCFG_A_Pos) | (attr.l << PMP_PMPCFG_L_Pos);
-		__set_PMPxCFG(i, pmpxcfg);
-	}
-}
 
 static const struct fdt_match spacemit_k3_mach[] = {
 	{ .compatible = "spacemit,k3" },
@@ -128,9 +44,6 @@ void boot_entry_dummy(unsigned long sc)
 	/* Turn off full address correlation check to improve L2 performance */
 	csr_clear(CSR_ML2HINT, CIU_CHR2_DEPD_DIS);
 	csr_set(CSR_ML2HINT, CIU_CHR2_MER_DIS);
-
-	/* set the pmp per-core */
-	spacemit_k3_pmp_init();
 
 	/* devote early */
 	spacemit_devote_pwrdown_cluster(current_hartid());
@@ -169,7 +82,7 @@ void boot_entry_dummy(unsigned long sc)
 
 static int spacemit_k3_early_init(bool cold_boot, const void *fdt, const struct fdt_match *match)
 {
-	int i;
+	int i, rc;
 	unsigned int hartid;
 	unsigned long cluster_id;
 	struct sbi_scratch *scratch;
@@ -244,6 +157,25 @@ static int spacemit_k3_early_init(bool cold_boot, const void *fdt, const struct 
 		/* enable dmasys clk for cpus reach all tcm range */
 		writel(1, (unsigned int *)DMASYS_CLK_EN);
 
+		/*
+		 * Protect reserved memory regions: no R/W/X for M/S/U modes.
+		 * ENF_PERMISSIONS locks the PMP entry so M-mode is also denied.
+		 * Protect the rcpu runtime environment from corruption
+		 */
+		rc = sbi_domain_root_add_memrange(RCPU0_RUNTIME_SPACE_BASE_ADDR, RCPU0_RUNTIME_SPACE_SIZE, 0x100000UL,
+						  SBI_DOMAIN_MEMREGION_ENF_PERMISSIONS);
+		if (rc)
+			return rc;
+
+		rc = sbi_domain_root_add_memrange(RCPU1_RUNTIME_SPACE_BASE_ADDR, RCPU1_RUNTIME_SPACE_SIZE, 0x100000UL,
+						  SBI_DOMAIN_MEMREGION_ENF_PERMISSIONS);
+		if (rc)
+			return rc;
+
+		rc = sbi_domain_root_add_memrange(RCPU_DTB_SPACE_BASE_ADDR, RCPU_DTB_SPACEMI_SIZE, 0x100000UL,
+						  SBI_DOMAIN_MEMREGION_ENF_PERMISSIONS);
+		if (rc)
+			return rc;
 	} else {
 		unsigned int current_hartid = current_hartid();
 
@@ -278,8 +210,6 @@ static bool spacemit_k3_cold_boot_allowed(u32 hartid, const struct fdt_match *ma
 		csr_clear(CSR_ML2HINT, CIU_CHR2_DEPD_DIS);
 		csr_set(CSR_ML2HINT, CIU_CHR2_MER_DIS);
 	}
-	/* set the pmp per-core */
-	spacemit_k3_pmp_init();
 
 	/* devote early */
 	spacemit_devote_pwrdown_cluster(hartid);
